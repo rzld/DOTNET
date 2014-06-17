a53aefdaebafa91d614ff6fbdbf582ec0da6eb54
margarethaWati <marpaungwati@gmail.com>
2014-06-17 11:48:37 +0700

    UnitTestTahap1
    Unit test percobaan untuk skor total di akhir apakah benar
    penghitungannya atau tidak

---
+++ IndonesiaPintar/IndonesiaPintar/Rules.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace IndonesiaPintar
+{
+    public class Rules
+    {
+        [TestMethod]
+        Public boolean checkResult(int result, int expectedResult)
+        {
+            Boolean accepted = false;
+            If (result==expectedResult)
+            {
+                accepted =true;
+            }
+            Return accepted;
+        }
+    }
+}
---
+++ IndonesiaPintar/IndonesiaPintar/UnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace IndonesiaPintar
+{
+    public class UnitTest
+    {
+        Public void testPenjumlahan()
+        {
+            Rules rules = new Rules();
+            int expectedResult = txtangka3.getText();//nilai angka yg diharapkan sbagai hasil, bisa inputan atau emang nilai konstan
+            
+            int c = SkorPemain //skor
+            boolean accepted = rules.checkResult(c, expectedResult);
+            If(accepted)
+            {
+                System.out.println("the result is as expected. Result ="+c+" and the expected result is "+expectedResult);
+            }
+            Else
+            {
+                System.out.println("the result is not as expected.Result ="+c+" and the expected result is "+expectedResult); 
+            }
+        }
+    }
+}
